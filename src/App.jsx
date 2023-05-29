1c0d0bd5499500127097ac0778361e71fe56a523
Júlia Valls Moix <juliavalls20@gmail.com>
2023-05-29 11:34:45 +0200

    handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function randomNumber() {
@@ -6,22 +7,34 @@ function randomNumber() {
 
 export default function App() {
 
+  const [firstNumber, setFirstNumber] = useState(randomNumber());
+  const [secondNumber, setSecondNumber] = useState(randomNumber());
+  const [inputNumber, setInputNumber] = useState('');
+  const handleClick = () => {
+    if (firstNumber + secondNumber === +inputNumber) {
+      console.log('Correct');
+    } else if (firstNumber + secondNumber !== inputNumber) {
+      console.log('Wrong');
+    }
+  }
+
   return (
     <div className="App">
       <div id="canvas">
-        <div id="primary-number">10</div>
+        <div id="primary-number">{firstNumber}</div>
         <div id="add">+</div>
-        <div id="secondary-number">7</div>
+        <div id="secondary-number">{secondNumber}</div>
         <div id="equal">=</div>
         <div>
           <input
             type="number"
             id="guess"
+            value={inputNumber}
+            onChange={(e) => setInputNumber(e.target.value)}
           />
         </div>
         <div>
-          <button id="btn" >
-            Check
+          <button id="btn" onClick={handleClick} > Check
           </button>
         </div>
       </div>
